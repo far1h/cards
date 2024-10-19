3d43d413227af113733d631f255481c2039b9b86
far1h <farihmuhammad45@gmail.com>
2024-10-19 12:21:43 +0700

    join a slice of strings

--- main.go
+++ main.go
@@ -3,9 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	// cards := newDeck()
-
-	// hand, remainingDeck := deal(cards, 5)
-	// hand.print()
-	// remainingDeck.print()
+	cards := newDeck()
+	fmt.Println(cards.toString())
 }
